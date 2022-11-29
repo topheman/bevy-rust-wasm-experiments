510ef25a305ae9af458185593adf5a9fc2894b38
Christophe Rosset <tophe@topheman.com>
2022-11-29 20:21:57 +0100

    feat(*): Bevy getting started ECS
    https://bevyengine.org/learn/book/getting-started/ecs/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use bevy::prelude::*;
+
+fn hello_world() {
+    println!("Hello World!")
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Alice".to_string())));
+    commands.spawn((Person, Name("Bob".to_string())));
+    commands.spawn((Person, Name("Eve".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello {}!", name.0);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_startup_system(add_people)
+        .add_system(hello_world)
+        .add_system(greet_people)
+        .run()
 }
