ce4853c3da7a36021025f7404cf60a458d3a6055
Christophe Rosset <tophe@topheman.com>
2022-12-01 19:05:33 +0100

    feat(bevy-get-started): add timer

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,17 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Eve".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello {}!", name.0);
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(query: Query<&Name, With<Person>>, time: Res<Time>, mut timer: ResMut<GreetTimer>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        println!("{:?}", time.elapsed());
+        for name in query.iter() {
+            println!("Hello {}!", name.0);
+        }
     }
 }
 
@@ -22,7 +30,9 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people).add_system(greet_people);
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
     }
 }
 
