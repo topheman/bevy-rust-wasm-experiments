15c91d66639d2dcfedaacd9e040c174150cc8928
Christophe Rosset <tophe@topheman.com>
2022-12-02 00:03:24 +0100

    feat(*): ball moves with arrow keys

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,16 @@ use bevy::{
     window::PresentMode,
 };
 
+// setup
+
 pub const CLEAR_COLOR: Color = Color::rgb(1.0, 0.0, 0.0);
+pub const PLAYER_SCALE: f32 = 1.0;
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
+
+// load resources
 
 #[derive(Resource)]
 struct BallTexture(Handle<TextureAtlas>);
@@ -32,6 +37,34 @@ fn load_assets(
     commands.insert_resource(BallTexture(texture_atlas_handle));
 }
 
+// setup player + texture
+
+#[derive(Component)]
+pub struct Player {
+    speed: f32,
+}
+
+fn player_movement(
+    mut player_query: Query<(&Player, &mut Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (player, mut transform) = player_query.single_mut();
+
+    if keyboard.pressed(KeyCode::Up) {
+        transform.translation.y += 1.0 * player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::Down) {
+        transform.translation.y -= 1.0 * player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::Left) {
+        transform.translation.x -= 1.0 * player.speed * time.delta_seconds();
+    }
+    if keyboard.pressed(KeyCode::Right) {
+        transform.translation.x += 1.0 * player.speed * time.delta_seconds();
+    }
+}
+
 fn spawn_player(mut commands: Commands, ball_texture: Res<BallTexture>) {
     let mut sprite = TextureAtlasSprite::new(1);
     sprite.color = Color::rgb(0.4, 0.9, 0.9);
@@ -42,12 +75,13 @@ fn spawn_player(mut commands: Commands, ball_texture: Res<BallTexture>) {
             texture_atlas: ball_texture.0.clone(),
             transform: Transform {
                 translation: Vec3::new(0.0, 0.0, 900.0), // 900.0 is like z-index
-                // scale: Vec3::splat(2.0),
+                scale: Vec3::splat(PLAYER_SCALE),
                 ..default()
             },
             ..default()
         })
-        .insert(Name::new("Player"));
+        .insert(Name::new("Player"))
+        .insert(Player { speed: 200.0 });
 }
 
 fn main() {
@@ -68,5 +102,6 @@ fn main() {
         .add_startup_system(setup)
         .add_startup_system(spawn_player)
         .add_startup_system_to_stage(StartupStage::PreStartup, load_assets)
+        .add_system(player_movement.label("movement"))
         .run()
 }
