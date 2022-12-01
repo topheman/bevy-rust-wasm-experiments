9e97f4cbbe78539d40c134451efe70a8a4f61671
Christophe Rosset <tophe@topheman.com>
2022-12-01 23:37:27 +0100

    feat(*): display a ball

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,72 @@
-use bevy::prelude::*;
+use bevy::{
+    // diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    // render::render_resource::Texture,
+    window::PresentMode,
+};
 
-#[derive(Component)]
-struct Person;
+pub const CLEAR_COLOR: Color = Color::rgb(1.0, 0.0, 0.0);
 
-#[derive(Component)]
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Alice".to_string())));
-    commands.spawn((Person, Name("Bob".to_string())));
-    commands.spawn((Person, Name("Eve".to_string())));
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
 
 #[derive(Resource)]
-struct GreetTimer(Timer);
+struct BallTexture(Handle<TextureAtlas>);
 
-fn greet_people(query: Query<&Name, With<Person>>, time: Res<Time>, mut timer: ResMut<GreetTimer>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        println!("{:?}", time.elapsed());
-        for name in query.iter() {
-            println!("Hello {}!", name.0);
-        }
-    }
+fn load_assets(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("ball-steel-no-shadow.png");
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(100.0, 110.0),
+        7,
+        1,
+        None,
+        Some(Vec2::new(-100.0, 0.0)),
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.insert_resource(BallTexture(texture_atlas_handle));
 }
 
-pub struct HelloPlugin;
+fn spawn_player(mut commands: Commands, ball_texture: Res<BallTexture>) {
+    let mut sprite = TextureAtlasSprite::new(1);
+    sprite.color = Color::rgb(0.4, 0.9, 0.9);
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
+    commands
+        .spawn(SpriteSheetBundle {
+            sprite,
+            texture_atlas: ball_texture.0.clone(),
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 900.0), // 900.0 is like z-index
+                // scale: Vec3::splat(2.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Name::new("Player"));
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .insert_resource(ClearColor(CLEAR_COLOR))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: 600.0,
+                height: 600.0,
+                title: "Bevy Rust Experiments".to_string(),
+                resizable: false,
+                cursor_visible: true,
+                present_mode: PresentMode::AutoVsync,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_startup_system(setup)
+        .add_startup_system(spawn_player)
+        .add_startup_system_to_stage(StartupStage::PreStartup, load_assets)
         .run()
 }
