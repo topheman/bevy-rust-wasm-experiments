c96ba01c0ac7a3e32922750bd0e0176555464556
Christophe Rosset <tophe@topheman.com>
2022-12-23 18:46:34 +0100

    feat(*): share styles between vite and wasm-server

--- www/main.js
+++ www/main.js
@@ -2,23 +2,10 @@ import './style.css'
 
 import init from './public/out/bevy-rust-wasm-experiments'
 
-function setup() {
-  console.log("setup")
-  init().then(module => {
-    // we never pass here, cause of
-    // Error: Using exceptions for control flow, don't mind me. This isn't actually an error!
-    console.log("resolved")
-    console.log("module", module)
-  }).catch(e => {
-    console.log("error")
-    console.log(e)
-  }).finally(() => {
+document.body.addEventListener('click', () => {
+  console.log('click')
+  init().then((e) => e).catch(e => e).finally(() => {
+    console.log('focus')
     document.querySelector('canvas').focus();
   })
-  document.body.classList.add('started');
-  window.removeEventListener('click', setup);
-  window.removeEventListener('touchstart', setup);
-}
-
-window.addEventListener('click', setup);
-window.addEventListener('touchstart', setup);
+}, { once: true });
