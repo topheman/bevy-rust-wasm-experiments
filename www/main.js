88ed803fde1a4292ed3f4c611ab0c6a72ac5e208
Christophe Rosset <tophe@topheman.com>
2022-12-23 19:33:54 +0100

    feat(*): start game directly after load

--- www/main.js
+++ www/main.js
@@ -2,10 +2,12 @@ import './style.css'
 
 import init from './public/out/bevy-rust-wasm-experiments'
 
-document.body.addEventListener('click', () => {
-  console.log('click')
-  init().then((e) => e).catch(e => e).finally(() => {
-    console.log('focus')
-    document.querySelector('canvas').focus();
-  })
-}, { once: true });
+// document.body.addEventListener('click', () => {
+//   console.log('click')
+//   init().then((e) => e).catch(e => e).finally(() => {
+//     console.log('focus')
+//     document.querySelector('canvas').focus();
+//   })
+// }, { once: true });
+
+init();
