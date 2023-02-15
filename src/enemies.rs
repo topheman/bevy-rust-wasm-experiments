6fe919a58aae6a0c1d24244e81f412a1a34cb26e
Christophe Rosset <tophe@topheman.com>
2023-02-15 23:14:44 +0100

    feat(*): never spawn more than MAX_ENEMIES

--- src/enemies.rs
+++ src/enemies.rs
@@ -9,6 +9,7 @@ use iyes_loopless::prelude::IntoConditionalSystem;
 pub const ENEMY_SCALE: f32 = 0.7;
 pub const BALL_DEFAULT_RADIUS: f32 = 50.0;
 pub const MIN_ENEMIES: i32 = 2;
+pub const MAX_ENEMIES: i32 = 10;
 
 pub struct EnemiesPlugin;
 
@@ -53,51 +54,59 @@ fn spawn_enemy(
     ball_texture: Res<BallTexture>,
     windows: Res<Windows>,
     query_player: Query<(&Ball, &Transform, With<Player>)>,
+    query_enemies: Query<(Entity, With<Enemy>)>,
     mut spawn_events: EventReader<EnemyEvents>,
 ) {
-    for event in spawn_events.iter() {
-        match event {
-            EnemyEvents::Spawn => {
-                let player = query_player.single();
-                let player_x = player.1.translation.x;
-                let player_y = player.1.translation.y;
-                let player_radius = player.0.radius;
-                let window = windows.get_primary().unwrap();
-                let (random_position, velocity_x, velocity_y) = get_random_position_and_speed(
-                    window.width(),
-                    window.height(),
-                    player_x - player_radius,
-                    player_x + player_radius,
-                    player_y - player_radius,
-                    player_y + player_radius,
-                    player_x,
-                    player_y,
-                    200.0,
-                );
-                let enemy_ball_component = (
-                    Ball::new(
-                        velocity_x,
-                        velocity_y,
-                        BALL_DEFAULT_RADIUS * ENEMY_SCALE,
-                        BallKind::Enemy,
-                    ),
-                    Enemy,
-                );
-                let player_entity = spawn_assets_sprite(
-                    &mut commands,
-                    &ball_texture,
-                    1,
-                    Color::rgb(0.4, 0.5, 0.5),
-                    random_position,
-                    Vec3::splat(ENEMY_SCALE),
-                );
+    let mut enemy_count = 0;
+    for _ in query_enemies.into_iter() {
+        enemy_count += 1;
+    }
 
-                commands
-                    .entity(player_entity)
-                    .insert(enemy_ball_component)
-                    .insert(Name::new("Enemy"));
+    if enemy_count < MAX_ENEMIES {
+        for event in spawn_events.iter() {
+            match event {
+                EnemyEvents::Spawn => {
+                    let player = query_player.single();
+                    let player_x = player.1.translation.x;
+                    let player_y = player.1.translation.y;
+                    let player_radius = player.0.radius;
+                    let window = windows.get_primary().unwrap();
+                    let (random_position, velocity_x, velocity_y) = get_random_position_and_speed(
+                        window.width(),
+                        window.height(),
+                        player_x - player_radius,
+                        player_x + player_radius,
+                        player_y - player_radius,
+                        player_y + player_radius,
+                        player_x,
+                        player_y,
+                        200.0,
+                    );
+                    let enemy_ball_component = (
+                        Ball::new(
+                            velocity_x,
+                            velocity_y,
+                            BALL_DEFAULT_RADIUS * ENEMY_SCALE,
+                            BallKind::Enemy,
+                        ),
+                        Enemy,
+                    );
+                    let player_entity = spawn_assets_sprite(
+                        &mut commands,
+                        &ball_texture,
+                        1,
+                        Color::rgb(0.4, 0.5, 0.5),
+                        random_position,
+                        Vec3::splat(ENEMY_SCALE),
+                    );
+
+                    commands
+                        .entity(player_entity)
+                        .insert(enemy_ball_component)
+                        .insert(Name::new("Enemy"));
+                }
+                _ => {}
             }
-            _ => {}
         }
     }
 }
