97352e9c963991bc316ce3517450fce481a90fff
Christophe Rosset <tophe@topheman.com>
2023-02-28 20:15:36 +0100

    feat(*): add social meta images

--- www/vite.config.js
+++ www/vite.config.js
@@ -1,5 +1,20 @@
-export default {
-  build: {
-    minify: false // temporary, wasm-bindgen js bindings must not be mangled
+import { defineConfig } from 'vite'
+
+const htmlPlugin = () => {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html) {
+      return html.replace(
+        /WEBSITE_BASE_PATH/g,
+        process.env.WEBSITE_BASE_PATH || "http://localhost:3000", // todo replace by final domain name
+      )
+    },
   }
 }
+
+export default defineConfig({
+  build: {
+    minify: false // temporary, wasm-bindgen js bindings must not be mangled
+  },
+  plugins: [htmlPlugin()]
+})
