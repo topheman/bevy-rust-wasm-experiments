36313827f16cc71a810b9316dd2f18c6d755ffac
Christophe Rosset <tophe@topheman.com>
2023-02-28 23:29:34 +0100

    chore(ci): TO REMOVE - disable wasm import while setting up CI

--- www/main.js
+++ www/main.js
@@ -1,10 +1,11 @@
 import './style.css'
 
-import init from './public/out/bevy-rust-wasm-experiments'
+// import init from './public/out/bevy-rust-wasm-experiments'
 import './public/components/footer-display'
 
 document.getElementById('start').addEventListener('click', () => {
-  console.log('start');
-  document.body.classList.add('playing');
-  init();
+  // console.log('start');
+  // document.body.classList.add('playing');
+  // init();
+  console.warn("wasm file import disabled while setting up CI");
 });
