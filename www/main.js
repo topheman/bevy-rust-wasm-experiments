49c5276fbb883ad59432823b8bacf901cd5f0941
Christophe Rosset <tophe@topheman.com>
2023-03-01 18:26:05 +0100

    Revert "chore(ci): TO REMOVE - disable wasm import while setting up CI"
    This reverts commit 36313827f16cc71a810b9316dd2f18c6d755ffac.

--- www/main.js
+++ www/main.js
@@ -1,11 +1,10 @@
 import './style.css'
 
-// import init from './public/out/bevy-rust-wasm-experiments'
+import init from './public/out/bevy-rust-wasm-experiments'
 import './public/components/footer-display'
 
 document.getElementById('start').addEventListener('click', () => {
-  // console.log('start');
-  // document.body.classList.add('playing');
-  // init();
-  console.warn("wasm file import disabled while setting up CI");
+  console.log('start');
+  document.body.classList.add('playing');
+  init();
 });
