923133eae774e6dafbdf2d5f855634ad85b0d9f1
Christophe Rosset <tophe@topheman.com>
2023-03-02 22:21:05 +0100

    feat(*): back to home page button

--- www/main.js
+++ www/main.js
@@ -12,3 +12,8 @@ document.getElementById('start').addEventListener('click', () => {
   document.body.classList.add('playing');
   init();
 });
+
+document.querySelector('.back-home-page').addEventListener('click', () => {
+  console.log('stop');
+  window.location.reload(); // todo find an more elegant and performant to cleanup wasm instanciations
+})
